a2ec43c4c82d095fb7c7c60ba7f79e7e05d65ff3
ikona113 <ikonabytex@gmail.com>
2025-04-24 18:06:08 +0300

    create

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -3,10 +3,6 @@ from page.homepage import HomePage
 
 def test_cart(driver):
     homepage = HomePage(driver)
-    homepage.open_luma()
+    homepage.open()
     homepage.accept_agree()
     homepage.click_argus()
-    homepage.click_size_radiant_tee()
-    homepage.click_gray_color_argus()
-    homepage.click_add_to_cart_radiant_tee()
-    homepage.display_cart_one_element()
